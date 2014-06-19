dba4bc3e4b01c0b24bac56cba822ed9a03cfade5
Sahil Arora <sahil.ec@gmail.com>
2014-06-18 22:16:50 -0700

    Version completed in class

--- src/com/codepath/loginscreen/LoginActivity.java
+++ src/com/codepath/loginscreen/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.codepath.loginscreen;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
+@SuppressLint("NewApi")
 public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		//LinearLayout ll = (LinearLayout) findViewById(R.id.box);
+		//ll.setAlpha((float) 0.80);
 	}
 }
